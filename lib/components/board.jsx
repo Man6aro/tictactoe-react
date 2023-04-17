e551079fbb015d4aa147b205e102951e644106dc
Frédéric Lang <f.lang@hotmail.fr>
2023-04-17 07:33:29 +0200

    add css styles

--- lib/components/board.jsx
+++ lib/components/board.jsx
@@ -1,7 +1,7 @@
 import { Square } from "./square";
 
 const Board = ({ squares, onClick }) => (
-  <div className="border-4 border-blue-900 rounded-lg grid grid-cols-3 h-64 w-64">
+  <div className="board">
     {squares.map((v, i) => (
       <Square key={i} value={v} onClick={() => onClick(i)} />
     ))}
--- lib/components/square.jsx
+++ lib/components/square.jsx
@@ -1,7 +1,9 @@
-const Square = ({onClick, value}) => (
-	<button onClick={onClick} className="border-2 border-blue-900 bg-blue-100 text-xl cursor-pointer font-black">
-        {value}
-    </button>
+import "./style.css";
+
+const Square = ({ onClick, value }) => (
+  <button onClick={onClick} className="square">
+    {value}
+  </button>
 );
 
-export {Square};
+export { Square };
