f117ba1c2f69e516c49cfd52c25dfcf2a8e01011
Frederic Lang <frederic1.lang.ext@orange.fr>
2023-04-26 19:40:33 +0200

    update styles

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,33 +6,14 @@ import { logger, Counter } from "lib";
 import { Game } from "lib/components/game";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log("Foo");
-  logger("bar");
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <Counter />
-      </div>
+      <h1>Playground</h1>
       <div className="card">
         <Game />
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Tic Tac Toe made with React
       </p>
     </div>
   );
